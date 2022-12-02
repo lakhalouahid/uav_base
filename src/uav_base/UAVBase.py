b2cb0f30f1d1585e623755a5ce20499b06c062ef
Ouahid LAK-HAL <ouahid.lak-hal@etu.uca.fr>
2022-12-02 09:20:14 +0000

    bug(queue cmd_pose)

--- src/uav_base/UAVBase.py
+++ src/uav_base/UAVBase.py
@@ -12,13 +12,14 @@ from nav_msgs.msg import Odometry
 
 
 class UAVBase():
-    def __init__(self, cmd_vel_topic, max_speed=Vector3(5, 5, 5)):
+    def __init__(self, cmd_vel_topic, max_speed=Vector3(5, 5, 5), delta=1e-4):
         self.position = Point()
         self.orientation = Quaternion()
         self.linear_velocity = Vector3()
         self.angular_velocity = Vector3()
         self.cmd_vel_pub = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
         self.max_speed = max_speed
+        self.delta = delta
 
     def update_state(self, state):
         self.position = state.pose.pose.position
@@ -28,15 +29,12 @@ class UAVBase():
 
     def move(self, position, orientation=Quaternion(0, 0, 0, 1), interval=1):
         ispositioned = False
-        while not ispositioned:
-            if self.dist_position(self, position, 3e-2):
-                ispositioned = True
+        for kk in range(3):
 
             dx, dy, dz = self.position.x - position.x, self.position.y - position.y, self.position.z - position.z
             dx, dy, dz = self.sym_clip(dx, self.max_speed.x), self.sym_clip(dy, self.max_speed.y), self.sym_clip(dz, self.max_speed.z)
 
             linear_velocity = Vector3(-dx / (1.5*interval), -dy / (1.5*interval), -dz / (1.5*interval))
-            print(linear_velocity)
 
             self.cmd_vel(Twist(linear_velocity, Vector3()), interval)
 
@@ -50,12 +48,12 @@ class UAVBase():
         rospy.sleep(interval)
 
     @staticmethod
-    def dist_position(uav, act_position, delta=1e-2):
-        dx = abs(uav.position.x - act_position.x)
-        dy = abs(uav.position.y - act_position.y)
-        dz = abs(uav.position.z - act_position.z)
-        speed = math.sqrt(uav.linear_velocity.x * uav.linear_velocity.x + uav.linear_velocity.y * uav.linear_velocity.y + uav.linear_velocity.z * uav.linear_velocity.z)
-        return max([dx, dy, dz]) <= delta and speed <= 0.5
+    def dist_position(cls, act_position, delta=1e-2):
+        dx = abs(cls.position.x - act_position.x)
+        dy = abs(cls.position.y - act_position.y)
+        dz = abs(cls.position.z - act_position.z)
+        speed = math.sqrt(cls.linear_velocity.x * cls.linear_velocity.x + cls.linear_velocity.y * cls.linear_velocity.y + cls.linear_velocity.z * cls.linear_velocity.z)
+        return max([dx, dy, dz]) <= cls.delta and speed <= 0.5
 
     @staticmethod
     def sym_clip(val, clip):
